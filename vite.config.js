6babbf688b5cbde76fb1b5e365ce4c62fc6a9a46
TomSif <thomas.sifferle.programmer@gmail.com>
2025-10-28 08:18:40 +0100

    fix(fonts):hébergement local des polices pour améliorer score lighthouse

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+    }),
+  ],
+  build: {
+    // Générer les preload hints pour les assets critiques
+    modulePreload: {
+      polyfill: true,
+    },
+    // Optimisation du CSS
+    cssCodeSplit: false, // Regroupe tout le CSS dans un seul fichier
+  },
 });
